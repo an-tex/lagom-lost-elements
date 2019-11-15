f4d960020fd6ba88973442002408c50f00b2228e
Andreas Gabor <andreas@rob.ag>
2019-11-15 14:57:02 +0100

    rather use wiretap than map

--- hello-world-stream-impl/src/main/scala/com/example/helloworldstream/impl/HelloWorldStreamServiceImpl.scala
+++ hello-world-stream-impl/src/main/scala/com/example/helloworldstream/impl/HelloWorldStreamServiceImpl.scala
@@ -1,5 +1,6 @@
 package com.example.helloworldstream.impl
 
+import akka.NotUsed
 import akka.stream.scaladsl.Source
 import com.example.helloworldstream.api.HelloWorldStreamService
 import com.lightbend.lagom.scaladsl.api.ServiceCall
@@ -14,10 +15,9 @@ class HelloWorldStreamServiceImpl extends HelloWorldStreamService {
 
   private final val log: Logger = LoggerFactory.getLogger(this.getClass)
 
-  def stream = ServiceCall { _ =>
-    Future.successful(Source(1.to(1000)).map { msg =>
-      log.info(s"sending $msg")
-      msg
-    })
+  def stream: ServiceCall[NotUsed, Source[Int, NotUsed]] = ServiceCall { _ =>
+    Future.successful(
+      Source(1.to(1000)).wireTap(msg => log.info(s"sending $msg"))
+    )
   }
 }
